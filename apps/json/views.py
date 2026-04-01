a3d83979eff2fc90854550b0586366c2909e5737
eshqulovmustafo <eshqulovmustafo@gmail.com>
2026-04-01 11:12:00 +0500

    fix api

--- apps/json/views.py
+++ apps/json/views.py
@@ -1,6 +1,5 @@
-from collections import defaultdict
-from django.db.models import Sum
-from django.db.models.functions import ExtractYear
+from django.db.models import F, Sum, Window
+from django.db.models.functions import RowNumber, ExtractYear
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from apps.json.models import RepositoryLanguage
@@ -13,16 +12,25 @@ class TopLanguagesByYearView(APIView):
             .annotate(year=ExtractYear('repo__created_at'))
             .values('year', 'language__name')
             .annotate(total_size=Sum('size'))
+            .annotate(
+                rank=Window(
+                    expression=RowNumber(),
+                    partition_by=[F('year')],
+                    order_by=F('total_size').desc()
+                )
+            )
+            .filter(rank__lte=5)
             .order_by('-year', '-total_size')
         )
 
-        data = defaultdict(list)
+        data = {}
         for item in queryset:
             year = item['year']
-            if len(data[year]) < 5:
-                data[year].append({
-                    'language': item['language__name'],
-                    'size': item['total_size']
-                })
+            if year not in data:
+                data[year] = []
+            data[year].append({
+                'language': item['language__name'],
+                'size': item['total_size']
+            })
 
         return Response(data)
